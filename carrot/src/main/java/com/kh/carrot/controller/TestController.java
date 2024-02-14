89f2f506e4765aa0bed34f371bb25bbdb2f6c7f5
bbongnam777 <bbongnam777@naver.com>
2024-02-14 10:58:33 +0900

    메세지는 정성스럽게

--- carrot/src/main/java/com/kh/carrot/controller/TestController.java
+++ carrot/src/main/java/com/kh/carrot/controller/TestController.java
@@ -7,26 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 
-	@RequestMapping("/zeze")
-	public String zeze() { 
-		return "Hello Git  !";
-	}
-
-
-
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello Git!!!";
 	}
-
-	
-
-	@RequestMapping("/yelimlee")
-	public String yelimlee() {
-		return "Hello Git!";
-	} 
-
-
-
 
 }
