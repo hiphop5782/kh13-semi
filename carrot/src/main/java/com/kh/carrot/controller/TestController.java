616db52cd06944d96126cbfd8fd49ed3e6fd6e68
SoyeonHyung <gudskirjs@naver.com>
2024-02-14 10:28:44 +0900

    testController구현

---
+++ carrot/src/main/java/com/kh/carrot/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.kh.carrot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/soyeon")
+	public String soyeon() {
+		return "Hello Git!";
+	}
+}
