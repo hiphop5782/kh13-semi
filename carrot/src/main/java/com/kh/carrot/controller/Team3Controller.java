373ac57ff1146cf74b97e07fcd1bc8e4cfba70a4
SoyeonHyung <gudskirjs@naver.com>
2024-02-14 11:16:18 +0900

    team3 컨트롤러 구현(형소연)

---
+++ carrot/src/main/java/com/kh/carrot/controller/Team3Controller.java
@@ -0,0 +1,13 @@
+package com.kh.carrot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Team3Controller {
+	
+	@RequestMapping("/soyeon")
+	public String home() {
+		return "Hello!";
+	}
+}
