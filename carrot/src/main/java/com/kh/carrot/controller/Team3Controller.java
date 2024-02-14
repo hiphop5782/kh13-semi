16aa8b5362d42e413d99dcd95e1ff216f3f1867e
jjh9696 <cnqzmf@naver.com>
2024-02-14 11:12:24 +0900

    Team3 컨트롤러 생성

--- carrot/src/main/java/com/kh/carrot/controller/Team3Controller.java
+++ carrot/src/main/java/com/kh/carrot/controller/Team3Controller.java
@@ -4,14 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
-	
-	@RequestMapping("/yeah2")
-	public String yeah2() {
-		return "Hello Git!";
-	}
-	@RequestMapping("/yeah3")
-	public String yeah3() {
+public class Team3Controller {
+
+	@RequestMapping("/Hi")
+	public String Hi() {
 		return "Hello Git!";
 	}
 }
