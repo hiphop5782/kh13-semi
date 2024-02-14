da44513a9d6b2b82cdef3a26142eb168d7bf625d
simcat13 <rycbar0@gmail.com>
2024-02-14 11:14:41 +0900

    test

---
+++ carrot/src/main/java/com/kh/carrot/controller/Team4Controller.java
@@ -0,0 +1,12 @@
+package com.kh.carrot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Team4Controller {
+	@RequestMapping("/cat")
+	public String cat() {
+		return "Hello Cat!";
+	}
+}
