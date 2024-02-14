3349380d95e8651add37e939eb9398d7c88a2f7d
hiphop5782 <zdlsz@hanmail.net>
2024-02-14 10:58:01 +0900

    실습 문제 풀이

---
+++ carrot/src/main/java/com/kh/carrot/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.kh.carrot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@RequestMapping("/kh")
+	public String kh() {
+		return "Hello Git!";
+	}
+}
